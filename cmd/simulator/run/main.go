1531ae0703b4d52ac20ae5707e414bf68926ab04
Seongjoo <seongjoow@gmail.com>
2024-07-11 21:24:45 +0900

    Feat: 시간에 대한 성능지표 그래프, arrival rate에 대한 성능지표 그래프 시각화

--- cmd/simulator/run/main.go
+++ cmd/simulator/run/main.go
@@ -266,7 +266,7 @@ func simulatorRandomPause() {
 	controlAllServers(&pubServers4, simulator.START)
 	controlAllServers(&pubServers5, simulator.START)
 
-	publisherCount := 20
+	publisherCount := 100
 	for i := 0; i < publisherCount; i++ {
 		startPubServers(&pubServerLoop, 60010, "50003")
 	}
